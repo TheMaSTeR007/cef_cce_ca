6d1be561e5e094328dd37e48b7390480a1f42f9e
Jaimin Gurjar <jaimingurjar007@gmail.com>
2024-12-30 14:11:19 +0530

    Commit on 30-12-2024

--- cef_cce_ca/spiders/cef_cce_ca_amp_pub.py
+++ cef_cce_ca/spiders/cef_cce_ca_amp_pub.py
@@ -1,10 +1,10 @@
 from scrapy.cmdline import execute
 from lxml.html import fromstring
-from unidecode import unidecode
 from datetime import datetime
 from typing import Iterable
 from scrapy import Request
 import pandas as pd
+import unicodedata
 import random
 import scrapy
 import evpn
@@ -43,28 +43,28 @@ def get_date_modified(selector, xpath_date_modified):
 
 
 def replace_with_na(text):
-    return re.sub(r'^[\s_-]+$', 'N/A', text)  # Replace _, __, -, --, --- with N/A
+    return re.sub(pattern=r'^[\s_-]+$', repl='N/A', string=text)  # Replace _, __, -, --, --- with N/A
 
 
-def remove_specific_punctuation(_text):
-    punctuation_marks = [
-        ".", ",", "?", "!", ":", ";", "—", "-", "_", "(", ")", "[", "]", "{", "}", '"', "'", "‘", "’", "“", "”", "«", "»",
-        "/", "\\", "|", "@", "#", "$", "%", "^", "&", "*", "+", "=", "~", "`", "<", ">", "…", "©", "®", "™"
-    ]
-    # Iterate over each punctuation mark and replace it in the original text
-    for punc_mark in punctuation_marks:
-        _text = _text.replace(punc_mark, f' ')
-        _text = remove_extra_spaces(_text=_text)
-    return _text
+def remove_punctuation(text):
+    if text == 'N/A':
+        return text
+    return ''.join(char for char in text if not (unicodedata.category(char).startswith('P') and char != '|'))
 
 
 # Function to remove Extra Spaces from Text
-def remove_extra_spaces(_text: str):
-    return ' '.join(_text.split())  # Remove extra spaces
+def remove_extra_spaces(text: str) -> str:
+    return re.sub(pattern=r'\s+', repl=' ', string=text).strip()  # Regular expression to replace multiple spaces and newlines with a single space
 
 
-def df_cleaner(data_frame):
+def remove_diacritics(input_str):
+    return input_str if input_str == 'N/A' else ''.join(char for char in unicodedata.normalize('NFD', input_str) if not unicodedata.combining(char))
+
+
+def df_cleaner(data_frame) -> pd.DataFrame:
     # Apply the function to all columns for Cleaning
+    data_frame = data_frame.astype(str)  # Convert all data to string
+    data_frame.drop_duplicates(inplace=True)  # Remove duplicate data from DataFrame
 
     for column in data_frame.columns:
         data_frame[column] = data_frame[column].astype(str).apply(replace_with_na)  # Convert to string before applying
@@ -72,11 +72,15 @@ def df_cleaner(data_frame):
         if 'date' in column:
             data_frame[column] = data_frame[column].apply(convert_date_format)
         elif 'name' in column:
-            data_frame[column] = data_frame[column].apply(remove_specific_punctuation)  # Remove punctuation
+            data_frame[column] = data_frame[column].apply(remove_punctuation)  # Remove punctuation
 
         data_frame[column] = data_frame[column].apply(remove_extra_spaces)  # Remove extra spaces
-        data_frame[column] = data_frame[column].apply(unidecode)  # Remove diacritics characters
+        data_frame[column] = data_frame[column].apply(remove_diacritics)  # Remove diacritics characters
     data_frame.drop(columns=['name_of_individual_corporation_or_entity'], errors='ignore', inplace=True)  # Remove column as 'name' already exists in another column
+    data_frame.replace(to_replace=r'^\s*$', value=None, regex=True, inplace=True)
+    data_frame.replace(to_replace='nan', value=pd.NA, inplace=True)  # After cleaning, replace 'nan' strings back with actual NaN values
+    data_frame.fillna(value='N/A', inplace=True)  # Replace NaN values with "N/A"
+    return data_frame
 
 
 class CefCceCaAmpPubSpider(scrapy.Spider):
@@ -87,19 +91,15 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
         print('Connecting to VPN (CANADA)')
         self.api = evpn.ExpressVpnApi()  # Connecting to VPN (CANADA)
         self.api.connect(country_id='79')  # canada country code
-        time.sleep(5)  # keep some time delay before starting scraping because connecting
-        if self.api.is_connected:
-            print('VPN Connected!')
-        else:
-            print('VPN Not Connected!')
+        time.sleep(10)  # keep some time delay before starting scraping because connecting
+        print('VPN Connected!' if self.api.is_connected else 'VPN Not Connected!')
 
-        self.final_data = list()
-        self.delivery_date = datetime.now().strftime('%Y%m%d')
+        self.final_data_list = list()
 
         # Path to store the Excel file can be customized by the user
         self.excel_path = r"../Excel_Files"  # Client can customize their Excel file path here (default: govtsites > govtsites > Excel_Files)
         os.makedirs(self.excel_path, exist_ok=True)  # Create Folder if not exists
-        self.filename = fr"{self.excel_path}/{self.name}_{self.delivery_date}.xlsx"  # Filename with Scrape Date
+        self.filename = fr"{self.excel_path}/{self.name}.xlsx"  # Filename with Scrape Date
 
         self.cookies_table_page = {
             'cookiesession1': '678B286DC8FB3CADC7C4041793DB9AD3',
@@ -118,7 +118,6 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
             's_tp': '6210',
             's_ppv': 'Publication%2520of%2520AMPs%2C78%2C44%2C4846%2C6%2C8',
         }
-
         self.cookies_data_page = {
             'cookiesession1': '678B286DC8FB3CADC7C4041793DB9AD3',
             'AMCVS_A90F2A0D55423F537F000101%40AdobeOrg': '1',
@@ -136,7 +135,6 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
             's_tp': '1319',
             's_ppv': 'Summary%2520of%2520the%2520Notice%2520of%2520Violation%2C95%2C95%2C1250.2000122070312%2C1%2C1',
         }
-
         self.headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
@@ -156,7 +154,7 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
 
     def start_requests(self) -> Iterable[Request]:
         yield scrapy.Request(url='https://www.cef-cce.ca/content.asp?section=amp&dir=pub&document=index&lang=e', cookies=self.cookies_table_page, headers=self.headers,
-                             method='GET', meta={'impersonate': random.choice(self.browsers)}, callback=self.parse)
+                             method='GET', meta={'impersonate': random.choice(self.browsers)}, callback=self.parse, dont_filter=True)
 
     def parse(self, response, **kwargs):
         selector = fromstring(response.text)
@@ -177,7 +175,6 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
                 data_rows = month_div.xpath('.//tr[position() > 2]')  # Get all rows after the header rows
                 for data_row in data_rows:
                     data_dict = {'url': response.url}
-                    # data_dict = {header: '' for header in headers}  # Initialize the dictionary
                     for header_index, header in enumerate(headers):
                         # Get the corresponding td for the header
                         td_xpath = f'.//td[position()={header_index + 1}]//text()'  # 1-based index
@@ -191,6 +188,7 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
                     data_dict['year'] = year
                     data_dict['month'] = month
 
+                    # Sending request on Detail page url
                     if data_dict['data_page_url'] != 'N/A':
                         yield scrapy.Request(url=data_dict['data_page_url'], headers=self.headers, cookies=self.cookies_data_page, cb_kwargs={'data_dict': data_dict},
                                              method='GET', meta={'impersonate': random.choice(self.browsers)}, callback=self.parse_data_page)
@@ -221,31 +219,36 @@ class CefCceCaAmpPubSpider(scrapy.Spider):
         data_dict['key_facts_of_violation'] = key_facts_text
 
         # Debugging output
-        print(data_dict)
-        self.final_data.append(data_dict)
+        # print(data_dict)
+        self.final_data_list.append(data_dict)
         print('-' * 100)
 
     def close(self, reason):
         print('closing spider...')
-        print("Converting List of Dictionaries into DataFrame then into Excel file...")
-        try:
-            print("Creating Excel sheet...")
-            native_df = pd.DataFrame(self.final_data)
-            native_df.drop_duplicates(inplace=True)  # Removing Duplicate data from DataFrame
-            native_df = native_df.astype(str)  # Convert all data to string
-            # normalize headers by joining each with '_' instead of blank-space and lowercase too
-            # Clean the column names
-            native_df.columns = [remove_specific_punctuation(col) for col in native_df.columns]  # Removing punctuation
-            # Convert the Index to a Series to use str methods
-            native_df.columns = pd.Series(native_df.columns).str.lower().str.replace(' ', '_')
+        if self.final_data_list:
+            print("Converting List of Dictionaries into DataFrame then into Excel file...")
+            try:
+                print("Creating Excel sheet...")
+                data_df = pd.DataFrame(self.final_data_list)
+                data_df.drop_duplicates(inplace=True)  # Removing Duplicate data from DataFrame
+                data_df = data_df.astype(str)  # Convert all data to string
+                # normalize headers by joining each with '_' instead of blank-space and lowercase too
+                # Clean the column names
+                data_df.columns = [remove_punctuation(col) for col in data_df.columns]  # Removing punctuation
+                # Convert the Index to a Series to use str methods
+                data_df.columns = pd.Series(data_df.columns).str.lower().str.replace(' ', '_')
 
-            # native_df.columns = [col.replace(',', '') for col in native_df.columns]  # Removing comma ',' from column names
-            df_cleaner(data_frame=native_df)  # Apply the function to all columns for Cleaning
-            with pd.ExcelWriter(path=self.filename, engine='xlsxwriter') as writer:
-                native_df.to_excel(excel_writer=writer, index=False)
-            print("Excel file Successfully created.")
-        except Exception as e:
-            print('Error while Generating Native Excel file:', e)
+                # native_df.columns = [col.replace(',', '') for col in native_df.columns]  # Removing comma ',' from column names
+                data_df = df_cleaner(data_frame=data_df)  # Apply the function to all columns for Cleaning
+                with pd.ExcelWriter(path=self.filename, engine='xlsxwriter', engine_kwargs={"options": {'strings_to_urls': False}}) as writer:
+                    data_df.insert(loc=0, column='id', value=range(1, len(data_df) + 1))  # Add 'id' column at position 1
+                    data_df.to_excel(excel_writer=writer, index=False)
+
+                print("Excel file Successfully created.")
+            except Exception as e:
+                print('Error while Generating Native Excel file:', e)
+        else:
+            print('Final-Data-List is empty, Hence not generating Excel File.')
         if self.api.is_connected:  # Disconnecting VPN if it's still connected
             self.api.disconnect()
 
